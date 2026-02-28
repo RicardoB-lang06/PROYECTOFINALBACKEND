905f2a4053ba2ae9051197b0d4df837e4683726d
RicardoB-lang06 <ricardobarandacisneros@gmail.com>
2026-02-28 10:11:39 -0600

    page3

--- src/routes/ingresos.routes.js
+++ src/routes/ingresos.routes.js
@@ -7,8 +7,11 @@ const router = express.Router();
 router.use(authMiddleware); 
 
 router.post('/', controller.create);
+
 router.get('/', controller.getAll);
-router.get('/', authMiddleware, getAllIngresos);
+
+router.get('/paginados', controller.getAllIngresos); 
+
 router.get('/:id', controller.getById);
 router.put('/:id', controller.update);
 router.delete('/:id', controller.remove);
